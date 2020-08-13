59736326b9aadf2ff713017e317b6ce55a7790cc
jfbm74 <jfbm74@gmail.com>
2020-08-12 19:08:41 -0500

    Fixing issue

--- models/state.py
+++ models/state.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from os import getenv
+from sqlalchemy import String, Column, ForeignKey
+from sqlalchemy.orm import relationship
+from models.city import City
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref='state')
+    else:
+        @property
+        def cities(self):
+            """Returns all cities in a State"""
+
+            return [city for city in models.storage.all(City).values()
+                    if city.state_id == self.id]
