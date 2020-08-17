1f2c07011646b80b37850efb2c42e4563cd20abc
David Corredor <vidmore8@gmail.com>
2020-08-17 12:41:06 -0500

    [FIX] - identation error

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -17,8 +17,8 @@ echo "<!DOCTYPE html>
     <p>Nginx html test file</p>
   </body>
 </html>" > /data/web_static/releases/test/index.html
-n -sf /data/web_static/releases/test /data/web_static/current
+ln -sf /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data
-sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+sudo sed -i '/listen 80 default_server/a \\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 service nginx restart
 exit 0
